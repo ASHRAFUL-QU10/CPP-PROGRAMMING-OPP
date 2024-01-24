e07e10a2b76313fe07d2f9fa656ecb4fba5bbe74
MUHAMMAD ASHRAFUL <106703215+ASHRAFUL-QU10@users.noreply.github.com>
2024-01-24 19:23:15 +0600

    Update Box It.cpp
    Update Box It C++

--- Box It.cpp
+++ Box It.cpp
@@ -1,14 +1,14 @@
 /*
 QU10
-Design a class named Box whose dimensions are integers and private to the class. The dimensions are labelled: length , breadth , and height .
+Design a class named Box whose dimensions are integers and private to the class. The dimensions are labeled: length, breadth, and height.
 
-The default constructor of the class should initialize , , and  to .
+The default constructor of the class should initialize, and  to.
 
-The parameterized constructor Box(int length, int breadth, int height) should initialize Box's  and  to length, breadth and height.
+The parameterized constructor Box(int length, int breadth, int height) should initialize Box's  to length, breadth, and height.
 
-The copy constructor BoxBox ) should set  and  to 's  and , respectively.
+The copy constructor BoxBox ) should set  and  to 's  and, respectively.
 
-Apart from the above, the class should have  functions:
+Apart from the above, the class should have  the following functions:
 
 int getLength() - Return box's length
 int getBreadth() - Return box's breadth
@@ -20,9 +20,9 @@ Overload the operator  for the class Box. Box   Box  if:
  <  and ==
  <  and == and ==
 Overload operator  for the class Box(). 
-If  is an object of class Box:
+If  it is an object of class Box:
 
- should print ,  and  on a single line separated by spaces.
+ should print,  and  on a single line separated by spaces.
 
 Constraints
 
